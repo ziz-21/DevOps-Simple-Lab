48df0bc1f5d6aeebaa897c8b9a5aaabb100315f9
ziz-21 <imzayed53@gmail.com>
2026-05-09 14:16:17 +0300

    Add files via upload

---
+++ test_calculator.py
@@ -0,0 +1,10 @@
+import unittest
+from calculator import add
+
+class TestCalculator(unittest.TestCase):
+    def test_add(self):
+        self.assertEqual(add(2, 3), 5)
+        self.assertEqual(add(-1, 1), 0)
+
+if __name__ == '__main__':
+    unittest.main()
